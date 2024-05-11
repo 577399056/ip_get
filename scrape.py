c09332bd37425734cf819f61175319779c2821f5
577399056 <51535035+577399056@users.noreply.github.com>
2024-05-12 00:52:05 +0800

    更新 scrape.py

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ timeout=10
 response = requests.get(url,timeout=timeout)
 # 获取网页内容
 html_content = response.text
-
+print(html_content)
 # 将内容保存到输出文件中
 with open('output.txt', 'w') as f:
     f.write(html_content)
