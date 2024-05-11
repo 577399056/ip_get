d6f8b93b9507d7772fc8fde37e6a7700bc823e04
577399056 <51535035+577399056@users.noreply.github.com>
2024-05-12 00:42:57 +0800

    更新

---
+++ scrape.py
@@ -0,0 +1,12 @@
+import requests
+
+# 获取网页内容
+url = 'https://ip.164746.xyz/'
+timeout=10
+response = requests.get(url,timeout=timeout)
+# 获取网页内容
+html_content = response.text
+
+# 将内容保存到输出文件中
+with open('output.txt', 'w') as f:
+    f.write(html_content)
