b68c9e0f2d8d888b0c5801b017067d85eca434fe
577399 <51535035+577399056@users.noreply.github.com>
2024-05-12 03:11:11 +0800

    更新 scrape.py

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ response = requests.get(url,timeout=timeout)
 # 获取网页内容
 html_content = response.text
 ip_addresses = re.findall(r">(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})<", html_content)
-#ip_addresses_joined = "\n".join(ip_addresses)
+ip_addresses_joined = "\n".join(ip_addresses)
 print(str(ip_addresses_joined))
 # 将内容保存到输出文件中
 #with open('ip.txt', 'w') as f:
